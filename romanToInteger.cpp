112d6708ccd47db08dd06b066e40fe7f0f2065ae
test <sebgillio@gmail.com>
2023-10-11 23:18:48 +0200

    romanToInteger

---
+++ romanToInteger.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <unordered_map>
+
+class Solution 
+{
+public:
+    int romanToInt(std::string s) 
+    {
+        std::unordered_map<char, int> romanToValue = 
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) 
+        {
+            int value = romanToValue[s[i]];
+            if (value < prevValue) 
+            {
+                result -= value;
+            } else 
+            {
+                result += value;
+            }
+            prevValue = value;
+        }
+
+        return result;
+    }
+};
