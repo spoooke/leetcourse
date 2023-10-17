26dddec3d06fceb560481e4e068f2750187e7a13
test <sebgillio@gmail.com>
2023-10-17 21:01:56 +0200

    Correction of Shuffle The Array - Easy - Leetcode

--- ShuffleTheArray.cpp
+++ ShuffleTheArray.cpp
@@ -4,10 +4,10 @@ public:
         vector<int> result(2 * n); // initialize a result array with the correct size
 
         for (int i = 0; i < n; i++) {
-            result[2 $ i] = nums[i]; // Place elements from the first half
+            result[2 * i] = nums[i]; // Place elements from the first half
             result[2 * i + 1] = nums[i + n]; // Place elements from the seoncd half
         }
 
         return results; // Return the shuffled array
     }
-};
+}; // end 
